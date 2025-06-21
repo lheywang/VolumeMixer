cd19d9dbdf60b937e5a53c046f16973750aa8cbf
lheywang <leonard.heywang@proton.me>
2025-06-21 17:59:08 +0200

    Tried some elements with USART_RTO. Does not work...

--- code/device_nucleo/Core/Src/main.c
+++ code/device_nucleo/Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -22,6 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "parser/parser.h"
+#include "int/int_uart.h"
 #include <stdio.h>
 #include <string.h>
 /* USER CODE END Includes */
@@ -70,13 +71,19 @@ void MX_CRC_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+DMA_HandleTypeDef hdma_usart2_rx; // Handle for USART2 RX DMA channel
+
+// Define a receive buffer and its size
+#define RX_BUFFER_SIZE 1024
+uint8_t RxBuffer[RX_BUFFER_SIZE];
+uint8_t TxBuffer[RX_BUFFER_SIZE]; // Buffer for transmitting messages back
 
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
 
@@ -108,9 +115,10 @@ int main(void)
   MX_USB_PCD_Init();
   MX_CRC_Init();
   /* USER CODE BEGIN 2 */
+  // Init ISR
+  isr_uart_init(&huart2);
 
   // struct CMD CMD_DATA;
-
 
   /* USER CODE END 2 */
 
@@ -119,145 +127,175 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	  ADC_ChannelConfTypeDef sConfig = {0}; // Structure to configure ADC channel
-	  int adc_value_ch0;
-	  int adc_value_ch1;
-	  char tx_buffer[128];
+    // ADC_ChannelConfTypeDef sConfig = {0}; // Structure to configure ADC channel
+    // int adc_value_ch0;
+    // int adc_value_ch1;
+    // char tx_buffer[128];
 
-	     /* --- Read ADC Channel 0 --- */
-	     sConfig.Channel = ADC_CHANNEL_1; // Select ADC Channel 0
-	     sConfig.Rank = ADC_REGULAR_RANK_1; // First conversion in sequence
-	     // Set sampling time; adjust based on your ADC clock and desired accuracy
-	     sConfig.SamplingTime = ADC_SAMPLETIME_19CYCLES_5;
-	     // Configure the selected channel
-	     if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-	     {
-	       Error_Handler();
-	     }
+    //    /* --- Read ADC Channel 0 --- */
+    //    sConfig.Channel = ADC_CHANNEL_1; // Select ADC Channel 0
+    //    sConfig.Rank = ADC_REGULAR_RANK_1; // First conversion in sequence
+    //    // Set sampling time; adjust based on your ADC clock and desired accuracy
+    //    sConfig.SamplingTime = ADC_SAMPLETIME_19CYCLES_5;
+    //    // Configure the selected channel
+    //    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+    //    {
+    //      Error_Handler();
+    //    }
 
-	     // Start ADC conversion
-	     if (HAL_ADC_Start(&hadc1) != HAL_OK)
-	     {
-	       Error_Handler();
-	     }
+    //    // Start ADC conversion
+    //    if (HAL_ADC_Start(&hadc1) != HAL_OK)
+    //    {
+    //      Error_Handler();
+    //    }
 
-	     // Poll for conversion completion (blocking mode)
-	     // Timeout of HAL_MAX_DELAY means it will wait indefinitely
-	     if (HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY) == HAL_OK)
-	     {
-	       adc_value_ch0 = HAL_ADC_GetValue(&hadc1); // Get the converted value
-	     }
-	     else
-	     {
-	       Error_Handler(); // Handle ADC conversion error
-	     }
+    //    // Poll for conversion completion (blocking mode)
+    //    // Timeout of HAL_MAX_DELAY means it will wait indefinitely
+    //    if (HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY) == HAL_OK)
+    //    {
+    //      adc_value_ch0 = HAL_ADC_GetValue(&hadc1); // Get the converted value
+    //    }
+    //    else
+    //    {
+    //      Error_Handler(); // Handle ADC conversion error
+    //    }
 
-	     // Stop ADC conversion after getting the value
-	     if (HAL_ADC_Stop(&hadc1) != HAL_OK)
-	     {
-	       Error_Handler();
-	     }
+    //    // Stop ADC conversion after getting the value
+    //    if (HAL_ADC_Stop(&hadc1) != HAL_OK)
+    //    {
+    //      Error_Handler();
+    //    }
 
-	     /* --- Read ADC Channel 1 --- */
-	     sConfig.Channel = ADC_CHANNEL_2; // Select ADC Channel 1
-	     sConfig.Rank = ADC_REGULAR_RANK_2; // First conversion in sequence
-	     // Set sampling time; adjust based on your ADC clock and desired accuracy
-	     sConfig.SamplingTime = ADC_SAMPLETIME_19CYCLES_5;
-	     // Configure the selected channel
-	     if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-	     {
-	       Error_Handler();
-	     }
+    //    /* --- Read ADC Channel 1 --- */
+    //    sConfig.Channel = ADC_CHANNEL_2; // Select ADC Channel 1
+    //    sConfig.Rank = ADC_REGULAR_RANK_2; // First conversion in sequence
+    //    // Set sampling time; adjust based on your ADC clock and desired accuracy
+    //    sConfig.SamplingTime = ADC_SAMPLETIME_19CYCLES_5;
+    //    // Configure the selected channel
+    //    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+    //    {
+    //      Error_Handler();
+    //    }
 
-	     HAL_Delay(100); // Add a small delay to avoid flooding the serial port
+    //    HAL_Delay(100); // Add a small delay to avoid flooding the serial port
 
-	     // Start ADC conversion
-	     if (HAL_ADC_Start(&hadc1) != HAL_OK)
-	     {
-	       Error_Handler();
-	     }
+    //    // Start ADC conversion
+    //    if (HAL_ADC_Start(&hadc1) != HAL_OK)
+    //    {
+    //      Error_Handler();
+    //    }
 
-	     // Poll for conversion completion (blocking mode)
-	     if (HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY) == HAL_OK)
-	     {
-	       adc_value_ch1 = HAL_ADC_GetValue(&hadc1); // Get the converted value
-	     }
-	     else
-	     {
-	       Error_Handler(); // Handle ADC conversion error
-	     }
+    //    // Poll for conversion completion (blocking mode)
+    //    if (HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY) == HAL_OK)
+    //    {
+    //      adc_value_ch1 = HAL_ADC_GetValue(&hadc1); // Get the converted value
+    //    }
+    //    else
+    //    {
+    //      Error_Handler(); // Handle ADC conversion error
+    //    }
 
-	     // Stop ADC conversion after getting the value
-	     if (HAL_ADC_Stop(&hadc1) != HAL_OK)
-	     {
-	       Error_Handler();
-	     }
+    //    // Stop ADC conversion after getting the value
+    //    if (HAL_ADC_Stop(&hadc1) != HAL_OK)
+    //    {
+    //      Error_Handler();
+    //    }
 
-	     /* --- Format and Transmit over UART --- */
-	     // Format the ADC values into a string
-	     // sprintf(buffer, "ADC0: %lu, ADC1: %lu\r\n", adc_value_ch0, adc_value_ch1);
-	     // Use %lu for unsigned long (uint32_t)
-	     int len = snprintf(tx_buffer, sizeof(tx_buffer), "ADC0: %lu, ADC1: %lu\r\n",
-	                        adc_value_ch0, adc_value_ch1);
-	     if (len < 0) {
-	         Error_Handler(); // Handle formatting error
-	     }
+    //    /* --- Format and Transmit over UART --- */
+    //    // Format the ADC values into a string
+    //    // sprintf(buffer, "ADC0: %lu, ADC1: %lu\r\n", adc_value_ch0, adc_value_ch1);
+    //    // Use %lu for unsigned long (uint32_t)
+    //    int len = snprintf(tx_buffer, sizeof(tx_buffer), "ADC0: %lu, ADC1: %lu\r\n",
+    //                       adc_value_ch0, adc_value_ch1);
+    //    if (len < 0) {
+    //        Error_Handler(); // Handle formatting error
+    //    }
 
-	     // Transmit the formatted string over UART
-	     if (HAL_UART_Transmit(&huart2, (uint8_t*)tx_buffer, strlen(tx_buffer), HAL_MAX_DELAY) != HAL_OK)
-	     {
-	       Error_Handler(); // Handle UART transmission error
-	     }
+    //    // Transmit the formatted string over UART
+    //    if (HAL_UART_Transmit(&huart2, (uint8_t*)tx_buffer, strlen(tx_buffer), HAL_MAX_DELAY) != HAL_OK)
+    //    {
+    //      Error_Handler(); // Handle UART transmission error
+    //    }
 
-	     HAL_Delay(400); // Add a small delay to avoid flooding the serial port
+    //    HAL_Delay(400); // Add a small delay to avoid flooding the serial port
 
-	  /*
-	  char Rx_data[1024] = {0}; // Buffer to store received byte
-	  char Tx_data[128] = {0};
+    char Rx_data[1024] = {0}; // Buffer to store received byte
+    char buf = '\0';
 
-	// Wait for one byte to be received via UART in blocking mode
-	// HAL_UART_Receive(huart_handle, pData, Size, Timeout)
-	// Here, we wait indefinitely (HAL_MAX_DELAY) for 1 byte.
-	if (HAL_UART_Receive(&huart2, (uint8_t*)Rx_data, 25, HAL_MAX_DELAY) == HAL_OK)
-	{
-	  // If a byte is received successfully, transmit it back immediately
-	  // HAL_UART_Transmit(huart_handle, pData, Size, Timeout)
-	  // Transmit the received byte in blocking mode.
-	  if (HAL_UART_Transmit(&huart2, (uint8_t*)Rx_data, 25, HAL_MAX_DELAY) != HAL_OK)
-	  {
-		Error_Handler(); // Handle transmission error
-	  }
+    int char_count = 0;
+    int buf_recv = 0;
 
-	  // Toggle a pin
-	  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_9);
+    while (buf_recv == 0 || char_count == 0)
+    {
+      int hal_retval = HAL_UART_Receive(&huart2, (uint8_t*)&buf, 1, 50);
 
-	  // Start parsing (works fine), now need to add the logic behind the screen and everything else
-	  int val = parser(Rx_data, &CMD_DATA);
+      if (hal_retval == HAL_OK)
+      {
+        Rx_data[char_count] = buf;
+        char_count += 1;
+      }
+      else if (hal_retval == HAL_TIMEOUT)
+      {
+        buf_recv = 1;
+      }
+      else {
+    	  Error_Handler();
+      }
+    }
 
-	  if (val != 0)
-	  {
-		  snprintf(Tx_data, 128, "\nParser error : %d", val);
-		  if (HAL_UART_Transmit(&huart2, (uint8_t*)Tx_data, 32, HAL_MAX_DELAY) != HAL_OK)
-		  {
-			Error_Handler(); // Handle transmission error
-		  }
-	  }
-	}
-	else
-	{
-	  Error_Handler(); // Handle reception error
-	}
-	}*/
+    // Then, show the buffer
+    if (HAL_UART_Transmit(&huart2, (uint8_t *)Rx_data, char_count, 50) != HAL_OK)
+    {
+      Error_Handler(); // Handle transmission error
+    }
+
+    /*
+    char Rx_data[1024] = {0}; // Buffer to store received byte
+    char Tx_data[128] = {0};
+
+  // Wait for one byte to be received via UART in blocking mode
+  // HAL_UART_Receive(huart_handle, pData, Size, Timeout)
+  // Here, we wait indefinitely (HAL_MAX_DELAY) for 1 byte.
+  if (HAL_UART_Receive(&huart2, (uint8_t*)Rx_data, 25, HAL_MAX_DELAY) == HAL_OK)
+  {
+    // If a byte is received successfully, transmit it back immediately
+    // HAL_UART_Transmit(huart_handle, pData, Size, Timeout)
+    // Transmit the received byte in blocking mode.
+    if (HAL_UART_Transmit(&huart2, (uint8_t*)Rx_data, 25, HAL_MAX_DELAY) != HAL_OK)
+    {
+    Error_Handler(); // Handle transmission error
+    }
+
+    // Toggle a pin
+    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_9);
+
+    // Start parsing (works fine), now need to add the logic behind the screen and everything else
+    int val = parser(Rx_data, &CMD_DATA);
+
+    if (val != 0)
+    {
+      snprintf(Tx_data, 128, "\nParser error : %d", val);
+      if (HAL_UART_Transmit(&huart2, (uint8_t*)Tx_data, 32, HAL_MAX_DELAY) != HAL_OK)
+      {
+      Error_Handler(); // Handle transmission error
+      }
+    }
+  }
+  else
+  {
+    Error_Handler(); // Handle reception error
+  }
+  }*/
     /* USER CODE BEGIN 3 */
 
-  /* USER CODE END 3 */
-
-}}
+    /* USER CODE END 3 */
+  }
+}
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -265,8 +303,8 @@ void SystemClock_Config(void)
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
@@ -280,9 +318,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -292,7 +329,7 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB|RCC_PERIPHCLK_ADC1;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB | RCC_PERIPHCLK_ADC1;
   PeriphClkInit.USBClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
   PeriphClkInit.Adc1ClockSelection = RCC_ADC1PLLCLK_DIV1;
 
@@ -303,10 +340,10 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief ADC1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief ADC1 Initialization Function
+ * @param None
+ * @retval None
+ */
 void MX_ADC1_Init(void)
 {
 
@@ -321,7 +358,7 @@ void MX_ADC1_Init(void)
   /* USER CODE END ADC1_Init 1 */
 
   /** Common config
-  */
+   */
   hadc1.Instance = ADC1;
   hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
   hadc1.Init.Resolution = ADC_RESOLUTION_12B;
@@ -342,7 +379,7 @@ void MX_ADC1_Init(void)
   }
 
   /** Configure Regular Channel
-  */
+   */
   sConfig.Channel = ADC_CHANNEL_1;
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SingleDiff = ADC_SINGLE_ENDED;
@@ -356,14 +393,13 @@ void MX_ADC1_Init(void)
   /* USER CODE BEGIN ADC1_Init 2 */
 
   /* USER CODE END ADC1_Init 2 */
-
 }
 
 /**
-  * @brief TSC Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TSC Initialization Function
+ * @param None
+ * @retval None
+ */
 void MX_TSC_Init(void)
 {
 
@@ -376,7 +412,7 @@ void MX_TSC_Init(void)
   /* USER CODE END TSC_Init 1 */
 
   /** Configure the TSC peripheral
-  */
+   */
   htsc.Instance = TSC;
   htsc.Init.CTPulseHighLength = TSC_CTPH_2CYCLES;
   htsc.Init.CTPulseLowLength = TSC_CTPL_2CYCLES;
@@ -389,7 +425,7 @@ void MX_TSC_Init(void)
   htsc.Init.SynchroPinPolarity = TSC_SYNC_POLARITY_FALLING;
   htsc.Init.AcquisitionMode = TSC_ACQ_MODE_NORMAL;
   htsc.Init.MaxCountInterrupt = DISABLE;
-  htsc.Init.ChannelIOs = TSC_GROUP6_IO1|TSC_GROUP6_IO2;
+  htsc.Init.ChannelIOs = TSC_GROUP6_IO1 | TSC_GROUP6_IO2;
   htsc.Init.ShieldIOs = 0;
   htsc.Init.SamplingIOs = TSC_GROUP6_IO4;
   if (HAL_TSC_Init(&htsc) != HAL_OK)
@@ -399,14 +435,13 @@ void MX_TSC_Init(void)
   /* USER CODE BEGIN TSC_Init 2 */
 
   /* USER CODE END TSC_Init 2 */
-
 }
 
 /**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART2 Initialization Function
+ * @param None
+ * @retval None
+ */
 void MX_USART2_UART_Init(void)
 {
 
@@ -434,14 +469,13 @@ void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
-
 }
 
 /**
-  * @brief USB Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USB Initialization Function
+ * @param None
+ * @retval None
+ */
 void MX_USB_PCD_Init(void)
 {
 
@@ -465,14 +499,13 @@ void MX_USB_PCD_Init(void)
   /* USER CODE BEGIN USB_Init 2 */
 
   /* USER CODE END USB_Init 2 */
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -488,13 +521,11 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, LD2_Pin|RED_Pin|GREEN_Pin|BLUE_Pin
-                          |LEDW1_Pin|LEDW2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, LD2_Pin | RED_Pin | GREEN_Pin | BLUE_Pin | LEDW1_Pin | LEDW2_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : LD2_Pin RED_Pin GREEN_Pin BLUE_Pin
                            LEDW1_Pin LEDW2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin|RED_Pin|GREEN_Pin|BLUE_Pin
-                          |LEDW1_Pin|LEDW2_Pin;
+  GPIO_InitStruct.Pin = LD2_Pin | RED_Pin | GREEN_Pin | BLUE_Pin | LEDW1_Pin | LEDW2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -516,7 +547,24 @@ void MX_CRC_Init(void)
 
   hcrc.Instance = CRC;
   HAL_CRC_Init(&hcrc);
+}
 
+/**
+ * @brief Enable DMA controller clock and configure streams.
+ * @param None
+ * @retval None
+ */
+void MX_DMA_Init(void)
+{
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE(); // Enable clock for DMA1 (or DMA2 if used by your MCU for USART2)
+
+  /* DMA interrupt init */
+  /* DMA1_Stream5_IRQn interrupt configuration for USART2_RX */
+  // IMPORTANT: Verify the correct Stream and IRQn for your specific MCU and USART2_RX
+  // On many STM32F4/L4, USART2_RX maps to DMA1 Stream 5.
+  HAL_NVIC_SetPriority(DMA1_Channel6_IRQn, 0, 0); // Set appropriate priority
+  HAL_NVIC_EnableIRQ(DMA1_Channel6_IRQn);
 }
 
 /* USER CODE BEGIN 4 */
@@ -524,9 +572,9 @@ void MX_CRC_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -538,14 +586,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
---
+++ code/device_nucleo/mixer/src/int/int_uart.c
@@ -0,0 +1,107 @@
+/** ================================================================
+ * @file    mixer/int/int_uart.c
+ *
+ * @brief   This file implement the interrupts procedures for the
+ * 			UART bus
+ *
+ * @date    21-06-2025
+ *
+ * @version 1.0.0
+ *
+ * @author  l.heywang (leonard.heywang@proton.me)
+ *
+ *  ================================================================
+ */
+/* -----------------------------------------------------------------
+ * INCLUDING LIBS
+ * -----------------------------------------------------------------
+ */
+// Header
+#include "int/int_uart.h"
+
+// ST HAL
+#include "main.h"
+
+extern UART_HandleTypeDef huart2;
+
+/* -----------------------------------------------------------------
+ * INT_UART
+ * -----------------------------------------------------------------
+ */
+
+int isr_uart_init(UART_HandleTypeDef *huartX)
+{
+	// First, enable the RTO interrupt on the STM32
+	// Timeout is set to 1000 bits, or about 2 ms.
+	huartX.Init.RTOEN = UART_RTOEN_ENABLE;
+	huartX.Init.ReceiverTimeout = 1000;
+	if (HAL_UART_Init(&huartX) != HAL_OK)
+	{
+	    Error_Handler();
+	}
+
+	// Enable the RTO interrupt in the UART peripheral
+	__HAL_UART_ENABLE_IT(&huartX, UART_IT_RTO);
+
+	// Configure the interrupt priority of the UART bus
+	HAL_NVIC_SetPriority(USART2_IRQn, 0, 0);
+	HAL_NVIC_EnableIRQ(USART2_IRQn);
+}
+
+void HAL_UART_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
+{
+  if (huart->Instance == USART2)
+  {
+    // Check if the event was an RTO (Receive Timeout)
+    // In many HAL versions, Size will correspond to the bytes received before RTO.
+    // You might also check huart->RxState == HAL_UART_STATE_READY if you want
+    // to differentiate between RTO and full buffer, though Size is usually enough.
+
+    // Process the received data up to 'Size' bytes.
+    // The data is in RxBuffer.
+    // For demonstration, let's echo the received message back to the serial terminal,
+    // prepending it with "RTO RX: " or "Full RX: ".
+
+    int len;
+    if (Size > 0 && Size <= RX_BUFFER_SIZE) // Ensure size is valid
+    {
+        // Null-terminate the received data for string functions
+        RxBuffer[Size] = '\0'; // Make sure the buffer has space for null terminator
+
+        // Check if it was an RTO (implies partial buffer or end of message)
+        // A common way to infer RTO is when the transfer stops before the full buffer is filled.
+        // The HAL_UART_RxEventCallback is triggered for RTO.
+        if (Size < RX_BUFFER_SIZE) { // If less than full buffer, it's likely an RTO or Idle Line
+            len = snprintf((char*)TxBuffer, sizeof(TxBuffer), "RTO RX (%u bytes): %s\r\n", Size, RxBuffer);
+        } else { // Full buffer received
+            len = snprintf((char*)TxBuffer, sizeof(TxBuffer), "Full RX (%u bytes): %s\r\n", Size, RxBuffer);
+        }
+
+        // Transmit the response
+        if (len > 0) {
+            HAL_UART_Transmit(&huart2, TxBuffer, len, HAL_MAX_DELAY);
+        }
+    }
+    else if (Size == 0)
+    {
+        // This might happen for some RTO/IDLE events if no data was actually received,
+        // or other specific edge cases depending on HAL version.
+        len = snprintf((char*)TxBuffer, sizeof(TxBuffer), "RTO/IDLE RX (0 bytes)\r\n");
+        HAL_UART_Transmit(&huart2, TxBuffer, len, HAL_MAX_DELAY);
+    }
+    else
+    {
+        // Handle unexpected size values, perhaps an overflow detection
+        len = snprintf((char*)TxBuffer, sizeof(TxBuffer), "Error: Invalid RX size %u\r\n", Size);
+        HAL_UART_Transmit(&huart2, TxBuffer, len, HAL_MAX_DELAY);
+    }
+
+
+    // IMPORTANT: Restart the DMA reception after processing the current data.
+    // The DMA transfer stops when an RTO or full buffer event occurs.
+    if (HAL_UART_Receive_DMA(&huart2, RxBuffer, RX_BUFFER_SIZE) != HAL_OK)
+    {
+      Error_Handler(); // Handle error if DMA reception cannot be restarted
+    }
+  }
+}
