981c142e5659efe0cad12fbb0eb3c2bda67836f5
lheywang <leonard.heywang@gmail.com>
2025-06-25 23:26:46 +0200

    Added a utils function to show an icon in console

---
+++ code/mixerlib/test/utils/screen_utils.hpp
@@ -0,0 +1,21 @@
+/*
+ * screen_utils.hpp
+ *
+ *  Created on: Jun 25, 2025
+ *      Author: user
+ */
+
+#ifndef UTILS_SCREEN_UTILS_HPP_
+#define UTILS_SCREEN_UTILS_HPP_
+
+#pragma once
+
+#include <stdint.h>
+
+int show_icon(uint8_t *buf);
+
+
+
+
+
+#endif /* UTILS_SCREEN_UTILS_HPP_ */
