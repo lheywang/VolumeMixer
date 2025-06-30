160108dd47da1a211938033013b8a3ec6ad5bbb8
lheywang <leonard.heywang@proton.me>
2025-06-30 17:29:18 +0200

    Added a digital filter for the raw ADC codes, to reduce noise

---
+++ code/mixerlib/Src/filter/filter.c
@@ -0,0 +1,49 @@
+/** ================================================================
+ * @file    filter/filter.c
+ *
+ * @brief   This file implement a digital filter on the software
+ * 			to even further smooth the noise of the ADC by itself.
+ *
+ * @date    30-06-2025
+ *
+ * @version 1.0.0
+ *
+ * @author  l.heywang (leonard.heywang@proton.me)
+ *
+ *  ================================================================
+ */
+
+/* -----------------------------------------------------------------
+ * INCLUDING LIBS
+ * -----------------------------------------------------------------
+ */
+// Header
+#include "filter/filter.h"
+
+/* -----------------------------------------------------------------
+ * DEFINES
+ * -----------------------------------------------------------------
+ */
+#define __FILTER_ALPHA2 (1 - FILTER_ALPHA)
+
+/* -----------------------------------------------------------------
+ * STATIC VARIABLES
+ * -----------------------------------------------------------------
+ */
+static uint16_t tmp[5] = {0};
+
+/* -----------------------------------------------------------------
+ * FUNCTIONS
+ * -----------------------------------------------------------------
+ */
+
+uint16_t filter(uint16_t input, int channel)
+{
+	if ((0 > channel) | (channel > 5))
+	{
+		return -1;
+	}
+
+	tmp[channel - 1] = (FILTER_ALPHA * input) + (__FILTER_ALPHA2 * tmp[channel - 1]);
+	return (tmp[channel - 1] > 4095) ? 4095 : tmp[channel - 1];
+}
